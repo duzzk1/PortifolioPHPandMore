f08645b7d07df4069b028a3d2fabf3abf3a23def
Eduardo Santos <eduluis.cco@gmail.com>
2023-05-15 22:08:02 -0300

    Create scrollreveal and click alert on linkedin button

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,54 @@
 let menuBtn = document.querySelector(".menu-toggle");
 let menuList = document.querySelector (".container-lista");
+let linkedinButton = document.querySelector(".linkedin-btn");
+let alertClick = document.querySelector(".alert-click");
+linkedinButton.addEventListener("click", ()=>{
+    alertClick.classList.toggle("alert-click-active");
+    alertClick.classList.toggle("alert-click");
+    setTimeout(function() {
+        alertClick.classList.toggle("alert-click-active");
+        alertClick.classList.toggle("alert-click");
+    }, 5000);
+})
 
 menuBtn.addEventListener ("click", () => {
     menuList.classList.toggle("active");
 })
+
+var slideLeft = {
+
+};
+ScrollReveal().reveal('.whois, .whois-desc, .whois-line',
+    {
+        delay: '100',
+        easing: "cubic-bezier(.6,-0.01,0,.98)",
+    distance: '150%',
+    duration: '1200',
+    origin: 'left',
+    });
+
+ScrollReveal().reveal('.linkedin-btn',
+    {
+        delay: '100',
+        easing: "cubic-bezier(.6,.09,.1,.97)",
+        distance: '250%',
+        duration: '3000',
+        origin: 'left',
+    });
+
+ScrollReveal().reveal('.projects',
+    {
+        delay: '100',
+        easing: "cubic-bezier(.6,.09,.1,.97)",
+        distance: '250%',
+        duration: '3200',
+        origin: 'bottom',
+    });
+
+ScrollReveal().reveal('.container-right',
+    {
+        delay: '1600',
+        duration: '2000',
+        scale: '0.3'
+    });
 
